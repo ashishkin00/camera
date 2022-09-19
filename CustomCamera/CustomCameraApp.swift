2afa97890488a3776521b534b2948e6f1e38dd58
Andrey Shishkin <a.shishkin@dunice.net>
2022-09-19 15:34:03 +0300

    Initial Commit

---
+++ CustomCamera/CustomCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomCameraApp.swift
+//  CustomCamera
+//
+//  Created by dunice-1 on 19.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct CustomCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
