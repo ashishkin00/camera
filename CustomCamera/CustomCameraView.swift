fa0d44f4afea31984dad9a63b26354a92443fead
Andrey Shishkin <a.shishkin@dunice.net>
2022-09-20 17:57:55 +0300

    Update

--- CustomCamera/CustomCameraView.swift
+++ CustomCamera/CustomCameraView.swift
@@ -2,27 +2,13 @@ import SwiftUI
 import AVFoundation
 
 struct CustomCameraView: View {
+    
     @StateObject var model = CustomCameraViewModel()
     var body: some View {
-        VStack {
-        UIPreview(previewLayer: $model.previewLayer)
+        Image(uiImage: model.image)
+            .ignoresSafeArea()
             .onAppear() {
                 model.begin()
             }
-            Button("asdasdas")
-            {
-                model.previewLayer.contents.
-            }
-        }
-    }
-}
-
-struct UIPreview: UIViewRepresentable {
-    @Binding var previewLayer: AVCaptureVideoPreviewLayer
-    func makeUIView(context: Context) -> some UIView {
-        UIView()
-    }
-    func updateUIView(_ uiView: UIViewType, context: Context) {
-        uiView.layer.addSublayer(previewLayer)
     }
 }
--- CustomCamera/CustomCameraViewModel.swift
+++ CustomCamera/CustomCameraViewModel.swift
@@ -4,6 +4,7 @@ import SwiftUI
 class CustomCameraViewModel: ObservableObject {
     @Published var captureSession = AVCaptureSession()
     @Published var previewLayer = AVCaptureVideoPreviewLayer()
+    @Published var image = UIImage(systemName: "camera")!
     func begin() {
         captureSession.beginConfiguration()
         
@@ -25,6 +26,24 @@ class CustomCameraViewModel: ObservableObject {
         previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
         previewLayer.videoGravity = .resizeAspect
         previewLayer.frame = UIScreen.main.bounds
+        
+        let diameter: CGFloat = 100
+        let rect = CGRect(origin: CGPoint.zero,
+                          size: CGSize(width: diameter, height: diameter))
+            
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.fillColor = UIColor.red.cgColor
+        shapeLayer.lineWidth = 10
+        shapeLayer.path = CGPath(ellipseIn: rect,
+                                 transform: nil)
+                
+        let renderer = UIGraphicsImageRenderer(size: rect.size)
+        
+        image = renderer.image {
+            context in
+
+            return shapeLayer.render(in: context.cgContext)
+        }
         
         captureSession.commitConfiguration()
         captureSession.startRunning()
